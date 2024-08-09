bbe41e7863c137125e24d928fbda57556410a40b
MaxDeadBear <85907140+MaxDeadBear@users.noreply.github.com>
2024-08-09 02:49:47 -0400

    Add files via upload

---
+++ custom-cursor.js
@@ -0,0 +1,7 @@
+document.addEventListener('mousedown', function() {
+    document.body.style.cursor = 'url("2.png"), auto';
+});
+
+document.addEventListener('mouseup', function() {
+    document.body.style.cursor = 'url("1.png"), auto';
+});
